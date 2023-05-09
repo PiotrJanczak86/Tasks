8474a58a912577ef7162ae1ab111f1047a99072c
PiotrJanczak86 <22085270-PiotrJanczak86@users.noreply.replit.com>
2023-05-09 03:37:07 +0000

    replit_commit_1

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,8 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/tasks")
